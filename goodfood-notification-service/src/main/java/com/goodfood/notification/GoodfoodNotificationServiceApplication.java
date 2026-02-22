9f8f30ccc592a0e47a75ea69319ed7eacb80cee4
Shubh-Namdev <s.namdev013@gmail.com>
2026-02-22 18:20:00 +0530

    initial commit for notification service

---
+++ goodfood-notification-service/src/main/java/com/goodfood/notification/GoodfoodNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goodfood.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodfoodNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodfoodNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ goodfood-notification-service/src/test/java/com/goodfood/notification/GoodfoodNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodfood.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodfoodNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
