acdb6dcb5072c9a9d50dadb989ec9a120cd3ce0e
Shubh-Namdev <s.namdev013@gmail.com>
2026-03-10 17:27:41 +0530

    problem fixed related to kafka order consumer in restaurant service

--- goodfood-order-service/src/main/java/com/goodfood/order/kafka/OrderEventProducer.java
+++ goodfood-order-service/src/main/java/com/goodfood/order/kafka/OrderEventProducer.java
@@ -1,18 +1,20 @@
 package com.goodfood.order.kafka;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.goodfood.order.events.OrderCreatedEvent;
 
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class OrderEventProducer {
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
 
     // public void sendOrderEvent(Map<String, Object> event) {
     //     kafkaTemplate.send("order-events", event);
--- goodfood-restaurant-service/src/main/java/com/goodfood/restaurant/config/KafkaConsumerConfigOrder.java
+++ goodfood-restaurant-service/src/main/java/com/goodfood/restaurant/config/KafkaConsumerConfigOrder.java
@@ -2,6 +2,7 @@ package com.goodfood.restaurant.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 
 import com.goodfood.restaurant.events.OrderCreatedEvent;
 
@@ -28,11 +30,14 @@ public class KafkaConsumerConfigOrder {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "restaurant-service-group-order");
+        // props.put(ConsumerConfig.GROUP_ID_CONFIG, "restaurant-service-group-order");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
         
         JsonDeserializer<OrderCreatedEvent> deserializer = new JsonDeserializer<>(OrderCreatedEvent.class);
         deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeHeaders(false);
         
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
